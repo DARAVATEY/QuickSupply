eb65f49a9a8a6dbf9300144d20b80a9de454dace
DARAVATEY <n.daravatey02@gmail.com>
2025-12-20 04:29:25 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css'; 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
+const container = document.getElementById('root');
+if (!container) throw new Error("Failed to find root element");
 
-ReactDOM.createRoot(rootElement).render(
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
