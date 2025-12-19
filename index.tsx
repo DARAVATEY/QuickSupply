47a5480dfa8923bf81c36082e36e95b8eada3c9b
DARAVATEY <n.daravatey02@gmail.com>
2025-12-20 04:51:28 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; 
 import App from './App';
+@import "tailwindcss";
 
 const container = document.getElementById('root');
 if (!container) throw new Error("Failed to find root element");
