07178ef47889b8bba03744aabc80bbbc668d1177
DARAVATEY <n.daravatey02@gmail.com>
2025-12-20 05:24:00 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'; // This is how you correctly load styles in React
+import './index.css'; 
 import App from './App';
 
 const container = document.getElementById('root');
-if (!container) throw new Error("Failed to find root element");
+if (!container) throw new Error("Root element not found");
 
-const root = createRoot(container);
-root.render(
+createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
